ac9c9c2872feadc6a62959e2c0d35d78c4b22fab
Benjamin Chu <benchu99@hotmail.com>
2023-05-28 14:47:05 -0700

    initial commit

---
+++ R/knockoffsr.R
@@ -0,0 +1,13 @@
+knockoff_setup <- function (pkg_check=TRUE,...){
+  julia <- JuliaCall::julia_setup(installJulia=TRUE,...)
+  if(pkg_check) JuliaCall::julia_install_package_if_needed("Knockoffs")
+  JuliaCall::julia_library("Knockoffs")
+
+  functions <- JuliaCall::julia_eval("filter(isascii, replace.(string.(propertynames(Knockoffs)),\"!\"=>\"_bang\"))")
+  ko <- julia_pkg_import("Knockoffs",functions)
+  JuliaCall::autowrap("GaussianKnockoff", fields = c("X̃","s","method","m"))
+  JuliaCall::autowrap("GaussianGroupKnockoff", fields = c("X", "groups", "S", "method", "m", "obj"))
+  JuliaCall::autowrap("LassoKnockoffFilter", fields = c("y","X", "m", "ko", "selected", "W"))
+  JuliaCall::autowrap("MarginalKnockoffFilter", fields = c("y","X","m", "ko", "selected", "W"))
+  ko
+}
